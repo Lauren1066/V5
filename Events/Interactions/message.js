556d9197387e4dbfb05060482ebd921fe8724d0c
AyrtonMead <121475513+AyrtonMead@users.noreply.github.com>
2023-02-05 00:50:35 +0000

    a
    a

--- Events/Interactions/message.js
+++ Events/Interactions/message.js
@@ -3,6 +3,10 @@ const { autoresponse } = require("../../Functions/Messages/autoresponse.js");
 const constantsFile = require("../../Storage/constants.js");
 const messageModel = require("../../Model/messages.js");
 const { modApplication } = require("../../Functions/Applications/mod.js");
+const { tutorApplication } = require("../../Functions/Applications/tutor.js");
+const {
+  eventmanagerApplication,
+} = require("../../Functions/Applications/eventmanager.js");
 const applicationModel = require("../../Model/applications.js");
 
 module.exports = {
@@ -13,11 +17,28 @@ module.exports = {
 
     // DMs
     if (message.channel.type === 1) {
-      const applicationData = await applicationModel.findOne({ memberID: message.author.id });
-      if (message.content.includes("apply")) {
-        message.reply("What team would you like to apply for:\nMod\nEvents\nTutor");
-      } else if (message.content.toLowerCase().includes("mod") || (applicationData && applicationData.type.toLowerCase() === "mod")) {
+      const applicationData = await applicationModel.findOne({
+        memberID: message.author.id,
+      });
+      if (message.content.toLowerCase().includes("apply")) {
+        message.reply(
+          "What team would you like to apply for:\nMod\nEvents\nTutor"
+        );
+      } else if (
+        message.content.toLowerCase().includes("mod") ||
+        (applicationData && applicationData.type.toLowerCase() === "mod")
+      ) {
         modApplication(message);
+      } else if (
+        message.content.toLowerCase().includes("tutor") ||
+        (applicationData && applicationData.type.toLowerCase() === "tutor")
+      ) {
+        tutorApplication(message);
+      } else if (
+        message.content.toLowerCase().includes("events") ||
+        (applicationData && applicationData.type.toLowerCase() === "events")
+      ) {
+        eventmanagerApplication(message);
       } else {
         return;
       }
@@ -35,7 +56,10 @@ module.exports = {
         xp(message);
       }
 
-      if (message.guild.id == constantsFile.mainServerID || message.guild.id == constantsFile.staffServerID) {
+      if (
+        message.guild.id == constantsFile.mainServerID ||
+        message.guild.id == constantsFile.staffServerID
+      ) {
         const data = await messageModel.findOne({
           memberID: message.author.id,
         });
