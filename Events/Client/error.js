0ff8f1100a1f67dc5845f9c3f9785c7d3f6635e2
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-02-11 02:49:22 -0500

    Removed Error Model

--- Events/Client/error.js
+++ Events/Client/error.js
@@ -1,18 +1,7 @@
-const errorModel = require("../../Model/errors.js");
-
 module.exports = {
   name: "error",
   once: false,
   async execute(error) {
-    const errorData = await errorModel.findOne({});
-    if (!errorData) {
-      const newError = new errorModel({
-        errorArray: [error],
-      });
-      newError.save();
-    } else {
-      errorData.errorArray.push(error);
-    }
     console.error(error);
   },
 };
--- Model/errors.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-const modelErrors = mongoose.Schema({
-  errorArray: [String],
-});
-module.exports = mongoose.model("modelErrors", modelErrors);
