1d46b0bb056fb0eec33f7cd454221c553b62b2fd
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-02-12 13:42:38 -0500

    Reset Api key and moved to config

--- Commands/Staff/restart.js
+++ Commands/Staff/restart.js
@@ -1,17 +1,17 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 const axios = require("axios");
+const config = require("../../Storage/config.json");
 module.exports = {
   data: new SlashCommandBuilder().setName("restart").setDescription("Restart the bot").setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
   async execute(interaction) {
     await interaction.reply("Attempting to restart...");
-    const apiKey = "ptlc_29ve5ypCP38L2dXffbTp0okKrGzlGhbEGdqnaRYlr4h";
     const url = "https://control.sparkedhost.us/api/client/servers/36ad70ce/power?signal=restart";
 
     axios({
       method: "POST",
       url: url,
       headers: {
-        Authorization: "Bearer " + apiKey,
+        Authorization: "Bearer " + config.apiKey,
         "Content-Type": "application/json",
         Accept: "application/json",
       },
