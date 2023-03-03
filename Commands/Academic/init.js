08ff4a5615b88e0f3cb4652e41dbfb8f976f9ac6
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-03-03 15:28:21 -0500

    Staff List Command

--- Commands/Academic/init.js
+++ Commands/Academic/init.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder().setName("init").setDescription("Send this after you finish helping someone!"),
+  data: new SlashCommandBuilder().setName("init").setDescription("For the open/close channel system!"),
   async execute(interaction) {
     interaction.channel.send("**Open Channels:**");
     interaction.channel.send("**Closed Channels:**");
---
+++ Commands/Staff/staffList.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const constantsFile = require("../../Storage/constants.js");
+module.exports = {
+  data: new SlashCommandBuilder().setName("stafflist").setDescription("See open positions"),
+  async execute(interaction) {
+    const guild = await interaction.client.guilds.fetch(constantsFile.staffServerID);
+    const jrModRole = await guild.roles.fetch("889258147259252816");
+    const modRole = await guild.roles.fetch("889258047317372948");
+    const srModRole = await guild.roles.fetch("1050264802871746600");
+    const adminRole = await guild.roles.fetch("889257914907373588");
+    const srAdminRole = await guild.roles.fetch("1050264785171791913");
+
+    const roles = [jrModRole, modRole, srModRole, adminRole, srAdminRole];
+    const max = ["4", "3", "1", "5", "1"];
+
+    const embed = new EmbedBuilder().setTitle("Moderation Team");
+
+    i = 0;
+    while (i < roles.length) {
+      await embed.addFields({ name: roles[i].name, value: `${roles[i].members.size}/${max[i]}` });
+      i++;
+    }
+
+    interaction.reply({ embeds: [embed] });
+  },
+};
