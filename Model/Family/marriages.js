4e163a78cdc5ee4d48bd9a671a470fd22eb3d0e0
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-03-04 10:36:59 -0500

    Remove unused files

--- Model/Family/marriages.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const marriages = mongoose.Schema({
-  users: [{ userOne: String, userTwo: String }],
-  proposedAt: Date,
-  accepted: Boolean,
-});
-module.exports = mongoose.model("marriages", marriages);
