e6ec298e6c9be859daf8c340d06744e564022172
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-03-04 22:15:18 -0500

    Use spread operator in constants file

--- Storage/constants.js
+++ Storage/constants.js
@@ -1,104 +1,69 @@
-// User IDS
-const clientID = "1012220507434786817";
-const ownerID = "693511698912641105";
+const ids = {
+  // User IDs
+  clientID: "1012220507434786817",
+  ownerID: "693511698912641105",
 
-// Server IDS
-const mainServerID = "1040773239607140485";
-const staffServerID = "888922290371330058";
-const testServerID = "980559928911618090";
+  // Server IDs
+  mainServerID: "1040773239607140485",
+  staffServerID: "888922290371330058",
+  testServerID: "980559928911618090",
 
-// Channel IDS
-const eventChannelID = "1040841716086874133";
-const statusChannel = "1073861685682638890";
-const mainGeneralChat = "1040773240135626764";
-const getHelpChannel = "1040792832832716800";
-const staffLBChannel = "913664976181403698";
-const mainLBChannel = "1040841716086874133";
-const staffBotChannel = "888923331489845278";
-const mainBotChannel = "1040786220487286825";
-const highStaffChannel = "1054150224387526746";
-const punishmentChannel = "1040794645883539516";
-const breakRequestChannel = "1067626214623293551";
-const breakLogsChannel = "1067633573215076392";
-const applicationLogChannel = "913671047847485470";
+  // Channel IDs
+  eventChannelID: "1040841716086874133",
+  statusChannel: "1073861685682638890",
+  mainGeneralChat: "1040773240135626764",
+  getHelpChannel: "1040792832832716800",
+  staffLBChannel: "913664976181403698",
+  mainLBChannel: "1040841716086874133",
+  staffBotChannel: "888923331489845278",
+  mainBotChannel: "1040786220487286825",
+  highStaffChannel: "1054150224387526746",
+  punishmentChannel: "1040794645883539516",
+  breakRequestChannel: "1067626214623293551",
+  breakLogsChannel: "1067633573215076392",
+  applicationLogChannel: "913671047847485470",
 
-// Category IDS
-const academicCategory = "1040785886264172564";
+  // Category IDs
+  academicCategory: "1040785886264172564",
+};
 
-// Role IDS
+const roles = {
+  // Ping Roles
+  deadChatRole: "1040837069834043442",
+  eventPingRole: "1040837359933063289",
+  giveawayPingRole: "1040838322890752034",
+  journalPingRole: "1040838589031911486",
+  questionPingRole: "1040839413242003466",
+  wordPingRole: "1040841007580856420",
+  memePingRole: "1063667481006321664",
+  announcementPings: "1046503072500813914",
+  factPingRole: "1070973110553169960",
+  applicationPingRole: "889269957882437692",
 
-// Ping Roles
-const deadChatRole = "1040837069834043442";
-const eventPingRole = "1040837359933063289";
-const giveawayPingRole = "1040838322890752034";
-const journalPingRole = "1040838589031911486";
-const questionPingRole = "1040839413242003466";
-const wordPingRole = "1040841007580856420";
-const memePingRole = "1063667481006321664";
-const announcementPings = "1046503072500813914";
-const factPingRole = "1070973110553169960";
-const applicationPingRole = "889269957882437692";
+  // Misc Roles
+  noCooldownRole: "1040834838690795560",
+  retiredStaffRole: "1046652860600942654",
+  mainStaffrole: "1040788165440577606",
+  canReadRole: "1064701602688159804",
 
-// Misc Roles
-const noCooldownRole = "1040834838690795560";
-const retiredStaffRole = "1046652860600942654";
-const mainStaffrole = "1040788165440577606";
-const canReadRole = "1064701602688159804";
-
-// Level Roles
-const levelonerole = "1040830062133530676";
-const levelfiverole = "1040830087307731005";
-const leveltenrole = "1040830091191652362";
-const leveltwentyrole = "1040830095243358340";
-const levelthirtyrole = "1040830099290857512";
-const levelfortyrole = "1040854023034634242";
-const levelfiftyrole = "1040830102814081065";
+  // Level Roles
+  levelonerole: "1040830062133530676",
+  levelfiverole: "1040830087307731005",
+  leveltenrole: "1040830091191652362",
+  leveltwentyrole: "1040830095243358340",
+  levelthirtyrole: "1040830099290857512",
+  levelfortyrole: "1040854023034634242",
+  levelfiftyrole: "1040830102814081065",
+};
 
 // XP parameters
-const minXP = 8;
-const maxXp = 12;
+const xp = {
+  minXP: 8,
+  maxXp: 12,
+};
 
 module.exports = {
-  academicCategory: academicCategory,
-  announcementPings: announcementPings,
-  applicationLogChannel: applicationLogChannel,
-  applicationPingRole: applicationPingRole,
-  breakLogsChannel: breakLogsChannel,
-  breakRequestChannel: breakRequestChannel,
-  canReadRole: canReadRole,
-  clientID: clientID,
-  deadChatRole: deadChatRole,
-  eventChannelID: eventChannelID,
-  eventPingRole: eventPingRole,
-  factPingRole: factPingRole,
-  getHelpChannel: getHelpChannel,
-  giveawayPingRole: giveawayPingRole,
-  highStaffChannel: highStaffChannel,
-  journalPingRole: journalPingRole,
-  levelfiftyrole: levelfiftyrole,
-  levelfiverole: levelfiverole,
-  levelfortyrole: levelfortyrole,
-  levelonerole: levelonerole,
-  leveltenrole: leveltenrole,
-  levelthirtyrole: levelthirtyrole,
-  leveltwentyrole: leveltwentyrole,
-  mainBotChannel: mainBotChannel,
-  mainGeneralChat: mainGeneralChat,
-  mainLBChannel: mainLBChannel,
-  mainServerID: mainServerID,
-  mainStaffrole: mainStaffrole,
-  maxXp: maxXp,
-  memePingRole: memePingRole,
-  minXP: minXP,
-  noCooldownRole: noCooldownRole,
-  ownerID: ownerID,
-  punishmentChannel: punishmentChannel,
-  questionPingRole: questionPingRole,
-  retiredStaffRole: retiredStaffRole,
-  staffBotChannel: staffBotChannel,
-  staffLBChannel: staffLBChannel,
-  staffServerID: staffServerID,
-  statusChannel: statusChannel,
-  testServerID: testServerID,
-  wordPingRole: wordPingRole,
+  ...ids,
+  ...roles,
+  ...xp,
 };
