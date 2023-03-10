f80b2bdb4933d395665ef5c0e0c0e9680b809634
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-03-10 00:43:08 -0500

    Update Cards

--- Functions/Levels/RankCards/backgroundCard.js
+++ Functions/Levels/RankCards/backgroundCard.js
@@ -3,41 +3,45 @@ const canvacord = require("canvacord");
 const { AttachmentBuilder } = require("discord.js");
 
 async function backgroundCard(levelData, backgroundData, user, expModel, message) {
-  expModel
-    .find({
-      guildID: constantsFile.mainServerID,
-    })
-    .sort({ level: -1, xp: -1 })
-    .exec((err, res) => {
-      i = 1;
-      res.forEach((member) => {
-        if (member.memberID == levelData.memberID) {
-          const xpNeeded = 5 * Math.pow(levelData.level, 2) + 60 * levelData.level + 100;
-          const rank = new canvacord.Rank()
-            .setAvatar(user.displayAvatarURL({ extension: "png" }))
-            .setCurrentXP(levelData.xp, backgroundData.color)
-            .setRequiredXP(xpNeeded, backgroundData.color)
-            .setCustomStatusColor(backgroundData.color)
-            .setProgressBar(backgroundData.color, "COLOR")
-            .setUsername(user.username, backgroundData.color)
-            .setLevel(levelData.level)
-            .setLevelColor(backgroundData.color, backgroundData.color)
-            .setRank(i)
-            .setRankColor(backgroundData.color, backgroundData.color)
-            .setOverlay(backgroundData.color, 0, false)
-            .setBackground("IMAGE", backgroundData.background)
-            .setDiscriminator(user.discriminator, backgroundData.color);
-          rank.build().then((data) => {
-            const attachment = new AttachmentBuilder(data, "RankCard.png");
-            message.channel.send({
-              content: `${user.username} has leveled up`,
-              files: [attachment],
-            });
-          });
-        } else {
-          i++;
-        }
-      });
-    });
+  try {
+    const res = await expModel
+      .find({
+        guildID: constantsFile.mainServerID,
+      })
+      .sort({ level: -1, xp: -1 })
+      .exec();
+
+    let i = 1;
+    for (const member of res) {
+      if (member.memberID == levelData.memberID) {
+        const xpNeeded = 5 * Math.pow(levelData.level, 2) + 60 * levelData.level + 100;
+        const rank = new canvacord.Rank()
+          .setAvatar(user.displayAvatarURL({ extension: "png" }))
+          .setCurrentXP(levelData.xp, backgroundData.color)
+          .setRequiredXP(xpNeeded, backgroundData.color)
+          .setCustomStatusColor(backgroundData.color)
+          .setProgressBar(backgroundData.color, "COLOR")
+          .setUsername(user.username, backgroundData.color)
+          .setLevel(levelData.level)
+          .setLevelColor(backgroundData.color, backgroundData.color)
+          .setRank(i)
+          .setRankColor(backgroundData.color, backgroundData.color)
+          .setOverlay(backgroundData.color, 0, false)
+          .setBackground("IMAGE", backgroundData.background)
+          .setDiscriminator(user.discriminator, backgroundData.color);
+        const data = await rank.build();
+        const attachment = new AttachmentBuilder(data, "RankCard.png");
+        await message.channel.send({
+          content: `${user.username} has leveled up`,
+          files: [attachment],
+        });
+        break;
+      } else {
+        i++;
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 module.exports = { backgroundCard };
--- Functions/Levels/RankCards/card.js
+++ Functions/Levels/RankCards/card.js
@@ -3,40 +3,39 @@ const { AttachmentBuilder } = require("discord.js");
 const canvacord = require("canvacord");
 
 async function card(levelData, user, expModel, message) {
-  expModel
-    .find({
-      guildID: constantsFile.mainServerID,
-    })
-    .sort({ level: -1, xp: -1 })
-    .exec((err, res) => {
-      i = 1;
-      res.forEach((member) => {
-        if (member.memberID == levelData.memberID) {
-          const xpNeeded = 5 * Math.pow(levelData.level, 2) + 60 * levelData.level + 100;
-          const rank = new canvacord.Rank()
-            .setAvatar(user.displayAvatarURL({ extension: "png" }))
-            .setCurrentXP(levelData.xp, "#ffffff")
-            .setRequiredXP(xpNeeded, "#ffffff")
-            .setCustomStatusColor("#ffffff")
-            .setProgressBar("#ffffff", "COLOR")
-            .setUsername(user.username, "#ffffff")
-            .setLevel(levelData.level)
-            .setLevelColor("#ffffff", "#ffffff")
-            .setRank(i)
-            .setRankColor("#ffffff", "#ffffff")
-            .setOverlay("#ffffff", 0, false)
-            .setBackground("COLOR", "#000001")
-            .setDiscriminator(user.discriminator, "#ffffff");
-          rank.build().then((data) => {
-            const attachment = new AttachmentBuilder(data, "RankCard.png");
-            message.channel.send({
-              files: [attachment],
-            });
-          });
-        } else {
-          i++;
-        }
-      });
-    });
+  try {
+    const leaderboard = await expModel
+      .find({
+        guildID: constantsFile.mainServerID,
+      })
+      .sort({ level: -1, xp: -1 });
+
+    const rank = leaderboard.findIndex((element) => element.memberID === levelData.memberID) + 1;
+
+    const xpNeeded = 5 * Math.pow(levelData.level, 2) + 60 * levelData.level + 100;
+
+    const image = await new canvacord.Rank()
+      .setAvatar(user.displayAvatarURL({ extension: "png" }))
+      .setCurrentXP(levelData.xp, "#ffffff")
+      .setRequiredXP(xpNeeded, "#ffffff")
+      .setCustomStatusColor("#ffffff")
+      .setProgressBar("#ffffff", "COLOR")
+      .setUsername(user.username, "#ffffff")
+      .setLevel(levelData.level)
+      .setLevelColor("#ffffff", "#ffffff")
+      .setRank(rank)
+      .setRankColor("#ffffff", "#ffffff")
+      .setOverlay("#ffffff", 0, false)
+      .setBackground("COLOR", "#000001")
+      .setDiscriminator(user.discriminator, "#ffffff")
+      .build();
+
+    const attachment = new AttachmentBuilder(image, "RankCard.png");
+    message.channel.send({ files: [attachment] });
+  } catch (err) {
+    console.error(err);
+    message.reply("Error retrieving leaderboard data.");
+  }
 }
+
 module.exports = { card };
