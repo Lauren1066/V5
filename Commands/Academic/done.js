74bc0f7f28d7aac5fb2bf0d554309a9f1eb1a13c
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-03-14 01:55:25 -0400

    Update done.js

--- Commands/Academic/done.js
+++ Commands/Academic/done.js
@@ -3,8 +3,9 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder().setName("done").setDescription("Standard Message"),
   async execute(interaction) {
-    interaction.reply(
+    interaction.channel.send(
       "If your done using this channel please use `/end` or, if not ignore this. If you decide to end please consider using `/rep` and `/review`."
     );
+    interaction.reply({ content: "Sent", ephemeral: true });
   },
 };
