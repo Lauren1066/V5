01169678a5d4ec901dc3c3ea4901129640371260
Lauren <61550272+Lauren1066@users.noreply.github.com>
2023-03-14 01:57:14 -0400

    Update done.js

--- Commands/Academic/done.js
+++ Commands/Academic/done.js
@@ -4,7 +4,7 @@ module.exports = {
   data: new SlashCommandBuilder().setName("done").setDescription("Standard Message"),
   async execute(interaction) {
     interaction.channel.send(
-      "If your done using this channel please use `/end` or, if not ignore this. If you decide to end please consider using `/rep` and `/review`."
+      "If you're done using this channel please use `/end` or, if not ignore this. If you decide to end please consider using `/rep` and `/review`."
     );
     interaction.reply({ content: "Sent", ephemeral: true });
   },
